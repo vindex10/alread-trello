efe9e6e7fe01f2f86e5ba21e73b6db0a187f7e07
Victor Ananyev <vindex10@gmail.com>
2019-09-19 23:01:39 +0200

    release script now deploys docs/ contents

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-git checkout gh-pages
-
 version=$1
 if [ -z $version ]; then
     echo "Specify version to release";
-    git describe --tags;
+    git describe --tags gh-pages;
     exit 1;
 fi;
 
-git merge --no-ff -m "release $version" master
-git tag "$version"
+set -x
+
+treeObjId=$(git write-tree --prefix=docs/)
+commitId=$(git commit-tree -p gh-pages -m "release $version" $treeObjId)
+git update-ref refs/heads/gh-pages $commitId
+git tag "$version" $commitId
+git checkout gh-pages
